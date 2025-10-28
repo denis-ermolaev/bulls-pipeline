2a766e791904447588dcf1be73fb9ae90c43160e
Denis Ermolaev <mrdenis.ppppr@mail.ru>
2025-10-28 13:06:30 +0300

    Second commit

--- run.py
+++ run.py
@@ -19,25 +19,18 @@ all_data = os.listdir(path_to_data)
 
 def create_vcf_for_sample(sample_id, sample_df, name_file):
     """
-    Создает VCF файл для одного образца на основе его данных в DataFrame.
+    Создает VCF файл для одного образца(организма) на основе его данных в DataFrame.
     """
     filepath = f"data_result/{name_file}/{sample_id}.vcf"
     output_path = Path(f"data_result/{name_file}")
 
-    # 3. Создаем директорию и все родительские директории, если их нет
     output_path.mkdir(parents=True, exist_ok=True)
 
-    # Формируем полный путь к файлу
-
-    # --- Шаг 1: Создание заголовка (Header) ---
     header = pysam.VariantHeader()
 
-    # Добавляем информацию о хромосомах (контигах)
-    # Берем уникальные хромосомы из данных этого образца
     for chrom in sample_df["Chr"].unique():
         header.add_meta("contig", items=[("ID", str(chrom))])
 
-    # Добавляем описание полей FORMAT, которые мы будем использовать
     header.add_meta(
         "FORMAT",
         items=[
@@ -48,15 +41,11 @@ def create_vcf_for_sample(sample_id, sample_df, name_file):
         ],
     )
 
-    # Добавляем имя нашего образца в заголовок
     header.add_sample(sample_id)
 
-    # --- Шаг 2: Создание VCF файла для записи ---
     vcf_out = pysam.VariantFile(filepath, "w", header=header)
 
-    # --- Шаг 3: Итерация по SNP и создание записей ---
     for row in sample_df.itertuples():
-        # Создаем новую запись. pysam использует 0-based координаты
         record = vcf_out.new_record(
             contig=str(row.Chr),
             start=row.Position,  # VCF POS (1-based) -> pysam start (0-based)
@@ -66,8 +55,7 @@ def create_vcf_for_sample(sample_id, sample_df, name_file):
 
         record.filter.add("PASS")
 
-        # --- Логика определения генотипа (самая важная часть) ---
-        # .SNP .IlmnStrand .RefStrand .REF .ALT
+        # --- ОПРЕДЕЛЕНИЕ ГЕНОТИПА ---
         complementarity = {"A": "T", "T": "A", "C": "G", "G": "C"}
         SNP = row.SNP.strip("[]").split("/")
         IlmnStrand = row.IlmnStrand
@@ -84,7 +72,7 @@ def create_vcf_for_sample(sample_id, sample_df, name_file):
             allele1 = 0 if SNP[row.Allele1_AB] == REF else 1
             allele2 = 0 if SNP[row.Allele2_AB] == REF else 1
             # Сортируем для создания нефазированного генотипа (0/1)
-            # Т.к неизвестно какой от Отца, а какой от матери
+            # Т.к неизвестно какой от отца, а какой от матери
             gt = tuple(sorted((allele1, allele2)))
             record.samples[sample_id]["GT"] = gt
         else:
@@ -102,6 +90,9 @@ def create_column_REF(row):
 
 
 def create_column_ALT(row):
+    """
+    Создание колонки с ALT аллелем
+    """
     complementarity = {"A": "T", "T": "A", "C": "G", "G": "C"}
     if pd.isnull(row["REF"]) or pd.isnull(row["SNP"]) or pd.isnull(row["RefStrand"]):
         return "DATA_ERROR"
@@ -110,20 +101,16 @@ def create_column_ALT(row):
     snp = row["SNP"].strip("[]").split("/")
     ref_strand = row["RefStrand"]
 
+    # ref_strand - указание для какой цепи ДНК(+-) указаны нуклеотиды в snp
     if ref_strand == "+":
         if ref_allele in snp:
             return snp[0] if snp[1] == ref_allele else snp[1]
         else:
             return "DATA_ERROR"
     elif ref_strand == "-":
-        # complement_ref = complementarity.get(ref_allele)
         snp = [complementarity[snp[0]], complementarity[snp[1]]]
         if ref_allele in snp:
             return snp[0] if snp[1] == ref_allele else snp[1]
-            # ALT для - цепи
-            # alt_on_minus_strand = snp[0] if snp[1] == complement_ref else snp[1]
-            # # ALT Для + цепи
-            # return complementarity.get(alt_on_minus_strand)
         else:
             return "DATA_ERROR"
     else:
@@ -132,10 +119,10 @@ def create_column_ALT(row):
 
 def process_file(name_file, path_to_data, bovinehd_manifest_df):
     """
-    Эта функция содержит всю логику для обработки ОДНОГО файла.
-    Она будет выполняться в отдельном процессе.
+    Обработка одного файла
     """
     try:
+        # pyarrow с 1 минуты 25 секунд до 25 секунд
         df = pd.read_csv(
             os.path.join(path_to_data, name_file),
             sep="\t",
